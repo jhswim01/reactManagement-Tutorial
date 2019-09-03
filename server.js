beb105845e178a2880f3c6b8b176d19beac6aa44
heeyeong <jhswim01@gmail.com>
2019-09-04 00:52:21 +0900

    create customer database table & node database connection setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+})
+connection.connect();
+
+
 app.get('/api/customers', (req,res) => {
-    res.send([
-            {
-              'id' : '1',
-              'image' : 'https://placeimg.com/64/64/1',
-              'name' : '다현',
-              'birthday' : '921021',
-              'gender' : '여자',
-              'job' : '학생'
-            },
-            {
-              'id' : '2',
-              'image' : 'https://placeimg.com/64/64/2',
-              'name' : '희영',
-              'birthday' : '921021',
-              'gender' : '남자',
-              'job' : '학생'
-            },
-            {
-              'id' : '3',
-              'image' : 'https://placeimg.com/64/64/3',
-              'name' : '희동',
-              'birthday' : '921021',
-              'gender' : '남자',
-              'job' : '개'
-            }
-    ]);
+    connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err, rows, fields) => {
+      res.send(rows);
+    } 
+  );
 });
 
 app.listen(port, () => console.log('Listening on port ${port}'));
